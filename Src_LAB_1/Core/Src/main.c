2b2e8ab575855259889b210e2cf7dea7860cd619
sonsonha <ha.danh2002@hcmut.edu.vn>
2023-09-21 10:39:28 +0700

    no message

--- Src_LAB_1/Core/Src/main.c
+++ Src_LAB_1/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,6 +84,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -131,6 +133,33 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, Seg1_Pin|Seg2_Pin|Seg3_Pin|Seg4_Pin
+                          |Seg5_Pin|Seg6_Pin|Seg7_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : Seg1_Pin Seg2_Pin Seg3_Pin Seg4_Pin
+                           Seg5_Pin Seg6_Pin Seg7_Pin */
+  GPIO_InitStruct.Pin = Seg1_Pin|Seg2_Pin|Seg3_Pin|Seg4_Pin
+                          |Seg5_Pin|Seg6_Pin|Seg7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
